1929e8f43f90619ecbec97e9c18a0f8012d961e7
hailangnoi1511@gmail.com <hailangnoi1511@gmail.com>
2023-03-19 17:08:00 +0700

    LAST COMMITED 19/3/2023

--- MovieTicket/src/code/Main.java
+++ MovieTicket/src/code/Main.java
@@ -37,9 +37,11 @@ public class Main {
 				} else if (numChoice == 3) {
 					System.out.println("--------XIN CHAO VA HEN GAP LAI !--------");
 					break;
+				} else {
+					System.out.println("LUA CHON CUA BAN KHONG CO TRONG MENU. VUI LONG NHAP LAI !");
 				}
 			} catch(NumberFormatException e) {
-				System.out.println("Ban nhap khong dung. Vui long nhap lai!");
+				System.out.println("BAN PHAI NHAP SO !");
 			}
 		}
 	}
@@ -62,27 +64,42 @@ public class Main {
 					System.out.print("- Nhap khung gio: ");
 					String time = sc.nextLine();
 					System.out.print("- Nhap phong: ");
-					String room = sc.nextLine();
-					System.out.print("- Nhap hang:");
+					String roomName = sc.nextLine();
+					System.out.print("- Nhap hang: ");
 					String row = sc.nextLine();
-					System.out.print("- Nhap so: ");
-					int num = sc.nextInt();
-					sc.nextLine();
-					if(checkTicket(listCinema, cinemaName, movieName, time, room, row, num)) {
+					int num;
+					while(true) {
+						System.out.print("- Nhap so: ");
+						String numString = sc.nextLine();
+						try {
+							num = Integer.parseInt(numString);
+							if(num < 1 || num > 8) {
+								throw new Exception();
+							}
+							break;
+						} catch(NumberFormatException e) {
+							System.out.println("BAN PHAI NHAP SO !");
+						} catch(Exception e) {
+							System.out.println("SO BAN NHAP PHAI TU 1-8 !");
+						}
+					}
+					if(checkTicket(cinemaName, movieName, time, roomName, row, num)) {
 						System.out.println("VE NAY HIEN VAN DANG CON !");
-						break;
+						menu1(sc);
 					} else {
 						System.out.println("VE NAY HIEN DA HET HOAC KHONG TON TAI !");
-						break;
+						menu1(sc);
 					}
 				} else if (numChoice == 2) {
 					bookTicketByCinema(sc);
-					break;
+					menu1(sc);
 				} else if (numChoice == 3) {
 					break;
+				} else {
+					System.out.println("LUA CHON CUA BAN KHONG CO TRONG MENU. VUI LONG NHAP LAI !");
 				}
 			} catch(NumberFormatException e) {
-				System.out.println("Ban nhap khong dung. Vui long nhap lai!");
+				System.out.println("BAN PHAI NHAP SO !");
 			}
 		}
 	}
@@ -109,12 +126,14 @@ public class Main {
 					findByAddress(sc);
 					menu2(sc);
 				} else if (numChoice == 4) {
-					menu3(sc);
+					menu2(sc);
 				} else if (numChoice == 5) {
 					break;
+				} else {
+					System.out.println("LUA CHON CUA BAN KHONG CO TRONG MENU. VUI LONG NHAP LAI !");
 				}
 			} catch(NumberFormatException e) {
-				System.out.println("Ban nhap khong dung. Vui long nhap lai!");
+				System.out.println("BAN PHAI NHAP SO !");
 			}
 		}
 	}
@@ -125,14 +144,14 @@ public class Main {
 			System.out.println("1. DAT 1 VE");
 			System.out.println("2. DAT NHIEU VE KHAC NHAU");
 			System.out.println("3. DAT NHIEU VE LIEN TIEP NHAU");
-			System.out.println("3. THOAT");
+			System.out.println("4. THOAT");
 			System.out.print("--> Nhap lua chon cua ban: ");
 			String choiceMenu = sc.nextLine();
 			try {
 				int numChoice = Integer.parseInt(choiceMenu);
 				if(numChoice == 1) {
 					bookTicketByCinema(sc);
-					menu2(sc);
+					menu3(sc);
 				} else if (numChoice == 2) {
 					System.out.println("- Ban muon dat bao nhieu ve ?");
 					int times = sc.nextInt();
@@ -149,22 +168,22 @@ public class Main {
 					} else {
 						System.out.println("BAN CHI DAT DUOC " + check + " VE !");
 					}
-					menu2(sc);
+					menu3(sc);
 				} else if (numChoice == 3) {
-					findByAddress(sc);
-					menu2(sc);
+					bookTicketsContinues(sc);
+					menu3(sc);
 				} else if (numChoice == 4) {
-					
-				} else if (numChoice == 5) {
 					break;
+				} else {
+					System.out.println("LUA CHON CUA BAN KHONG CO TRONG MENU. VUI LONG NHAP LAI !");
 				}
 			} catch(NumberFormatException e) {
-				System.out.println("Ban nhap khong dung. Vui long nhap lai!");
+				System.out.println("BAN PHAI NHAP SO !");
 			}
 		}
 	}
 	
-	public static String showListMovie(List<Cinema> listCinema, String cinemaName) {
+	public static String showListMovie(String cinemaName) {
 		List<String> listString = new ArrayList<>();
 		for(Cinema cinema : listCinema) {
 			if(cinema.getCinemaName().equals(cinemaName)) {
@@ -184,7 +203,7 @@ public class Main {
 		return str;
 	}
 	
-	public static String showListMovieTime(List<Cinema> listCinema, String cinemaName, String movieName) {
+	public static String showListMovieTime(String cinemaName, String movieName) {
 		String str = "";
 		for(Cinema cinema : listCinema) {
 			if(cinema.getCinemaName().equals(cinemaName)) {
@@ -200,7 +219,7 @@ public class Main {
 		return str;
 	}
 	
-	public static void showListSeat(List<Cinema> listCinema, String cinemaName, String movieName, String tim, String roomName) {
+	public static void showListSeat(String cinemaName, String movieName, String tim, String roomName) {
 		for(Cinema cinema : listCinema) {
 			if(cinema.getCinemaName().equals(cinemaName)) {
 				for(Room room : cinema.getListRoom()) {
@@ -216,7 +235,7 @@ public class Main {
 		}
 	}
 	
-	public static void removeTicket(List<Cinema> listCinema, String cinemaName, String movieName, String tim, String roomName, String row, int number) {
+	public static void removeTicket(String cinemaName, String movieName, String tim, String roomName, String row, int number) {
 		for(int i = 0; i < listCinema.size(); i++) {
 			if(listCinema.get(i).getCinemaName().equals(cinemaName)) {
 				for(int j = 0; j < listCinema.get(i).getListRoom().size(); j++) {
@@ -240,7 +259,7 @@ public class Main {
 		}
 	}
 	
-	public static boolean checkTicket(List<Cinema> listCinema, String cinemaName, String movieName, String tim, String roomName, String row, int number) {
+	public static boolean checkTicket(String cinemaName, String movieName, String tim, String roomName, String row, int number) {
 		for(int i = 0; i < listCinema.size(); i++) {
 			if(listCinema.get(i).getCinemaName().equals(cinemaName)) {
 				for(int j = 0; j < listCinema.get(i).getListRoom().size(); j++) {
@@ -269,24 +288,37 @@ public class Main {
 		System.out.print("- Ten rap (Beta/Lotte/CGV) : ");
 		String cinemaName = sc.nextLine();
 		System.out.println("Cac phim hien co: ");
-		System.out.print(showListMovie(listCinema, cinemaName));
+		System.out.print(showListMovie(cinemaName));
 		System.out.print("- Nhap ten phim: ");
 		String movieName = sc.nextLine();
 		System.out.println("Cac khung gio chieu: ");
-		System.out.print(showListMovieTime(listCinema, cinemaName, movieName));
+		System.out.print(showListMovieTime(cinemaName, movieName));
 		System.out.print("- Nhap khung gio: ");
 		String time = sc.nextLine();
 		System.out.print("- Nhap phong: ");
-		String room = sc.nextLine();
+		String roomName = sc.nextLine();
 		System.out.println("Danh sach ghe con trong:" );
-		showListSeat(listCinema, cinemaName, movieName, time, room);
+		showListSeat(cinemaName, movieName, time, roomName);
 		System.out.print("- Nhap hang: ");
 		String row = sc.nextLine();
-		System.out.print("- Nhap so: ");
-		int number = sc.nextInt();
-		sc.nextLine();
-		if (checkTicket(listCinema, cinemaName, movieName, time, room, row, number)) {
-			removeTicket(listCinema, cinemaName, movieName, time, room, row, number);
+		int num;
+		while(true) {
+			System.out.print("- Nhap so: ");
+			String numString = sc.nextLine();
+			try {
+				num = Integer.parseInt(numString);
+				if(num < 1 || num > 8) {
+					throw new Exception();
+				}
+				break;
+			} catch(NumberFormatException e) {
+				System.out.println("BAN PHAI NHAP SO !");
+			} catch(Exception e) {
+				System.out.println("SO BAN NHAP PHAI TU 1-8 !");
+			}
+		}
+		if (checkTicket(cinemaName, movieName, time, roomName, row, num)) {
+			removeTicket(cinemaName, movieName, time, roomName, row, num);
 			System.out.println("DAT VE THANH CONG !");
 			return true;
 		} else {
@@ -302,7 +334,7 @@ public class Main {
 		return false;
 	}
 	
-	public static String showByMovie(List<Cinema> listCinema, String movieName) {
+	public static String showByMovie(String movieName) {
 		String str = "";
 		for(int i = 0; i < listCinema.size(); i++) {
 			for(int j = 0; j < listCinema.get(i).getListRoom().size(); j++) {
@@ -319,7 +351,7 @@ public class Main {
 		return str;
 	}
 	
-	public static String showByCinema(List<Cinema> listCinema, String cinemaName) {
+	public static String showByCinema(String cinemaName) {
 		String str = "";
 		for(int i = 0; i < listCinema.size(); i++) {
 			if(listCinema.get(i).getCinemaName().equals(cinemaName)) {
@@ -337,7 +369,7 @@ public class Main {
 		return str;
 	}
 	
-	public static String showByAddress(List<Cinema> listCinema, String address) {
+	public static String showByAddress(String address) {
 		String str = "";
 		for(int i = 0; i < listCinema.size(); i++) {
 			if(listCinema.get(i).getAddress().equals(address)) {
@@ -359,21 +391,126 @@ public class Main {
 		System.out.print("- Ten phim: ");
 		String movieName = sc.nextLine();
 		System.out.println("Cac rap hien chieu: ");
-		System.out.print(showByMovie(listCinema, movieName));
+		System.out.print(showByMovie(movieName));
 	}
 	
 	public static void findByCinema(Scanner sc) {
 		System.out.print("- Ten rap: ");
 		String cinemaName = sc.nextLine();
 		System.out.println("Cac phim hien chieu: ");
-		System.out.print(showByCinema(listCinema, cinemaName));
+		System.out.print(showByCinema(cinemaName));
 	}
 	
 	public static void findByAddress(Scanner sc) {
 		System.out.print("- Dia Chi: ");
 		String address = sc.nextLine();
 		System.out.println("Cac rap hien chieu: ");
-		System.out.print(showByAddress(listCinema, address));
+		System.out.print(showByAddress(address));
 	}
 	
+	public static void bookTicketsContinues(Scanner sc) {
+		System.out.print("- Ten rap (Beta/Lotte/CGV) : ");
+		String cinemaName = sc.nextLine();
+		System.out.println("Cac phim hien co: ");
+		System.out.print(showListMovie(cinemaName));
+		System.out.print("- Nhap ten phim: ");
+		String movieName = sc.nextLine();
+		System.out.println("Cac khung gio chieu: ");
+		System.out.print(showListMovieTime(cinemaName, movieName));
+		System.out.print("- Nhap khung gio: ");
+		String time = sc.nextLine();
+		System.out.print("- Nhap phong: ");
+		String roomName = sc.nextLine();
+		System.out.println("- Ban muon dat bao nhieu ghe lien tiep ?");
+		int number = sc.nextInt();
+		sc.nextLine();
+		System.out.println("Cac hang ghe co " + number + " ghe lien tiep:");
+		showSeatsContinues(cinemaName, movieName, time, roomName, number);
+		System.out.println("- Chon Hang ghe ban muon dat ?");
+		String row = sc.nextLine();
+		System.out.println("- Ban muon dat tu ghe so may ?");
+		int numSeat;
+		while(true) {
+			System.out.print("- Nhap so: ");
+			String numString = sc.nextLine();
+			try {
+				numSeat = Integer.parseInt(numString);
+				if(numSeat < 1 || numSeat > 8) {
+					throw new Exception();
+				}
+				break;
+			} catch(NumberFormatException e) {
+				System.out.println("BAN PHAI NHAP SO !");
+			} catch(Exception e) {
+				System.out.println("SO BAN NHAP PHAI TU 1-8 !");
+			}
+		}
+		bookSeatsContinues(cinemaName, movieName, time, roomName, number, row, numSeat);
+		
+	}
+	
+	public static void showSeatsContinues(String cinemaName, String movieName, String tim, String roomName, int number) {
+		for(Cinema cinema : listCinema) {
+			if(cinema.getCinemaName().equals(cinemaName)) {
+				for(Room room : cinema.getListRoom()) {
+					if(room.getRoomName().equals(roomName)) {
+						for(Time time : room.getListTime()) {
+							if(time.getTime().equals(tim) && time.getTicket().getMovieName().equals(movieName)) {
+								for(int i = 0; i < time.getTicket().getListSeat().size(); i++) {
+									int cnt = 1;
+									for(int j = 0; j < time.getTicket().getListSeat().get(i).size() - 1; j++) {
+										if(time.getTicket().getListSeat().get(i).get(j+1).getNumber() - time.getTicket().getListSeat().get(i).get(j).getNumber() == 1) {
+											cnt++;
+											if(cnt == number) {
+												System.out.println(time.getTicket().getListSeat().get(i));
+												break;
+											}
+										} else {
+											cnt = 1;
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+	
+	public static void bookSeatsContinues(String cinemaName, String movieName, String tim, String roomName, int number, String row, int numSeat) {
+		for(Cinema cinema : listCinema) {
+			if(cinema.getCinemaName().equals(cinemaName)) {
+				for(Room room : cinema.getListRoom()) {
+					if(room.getRoomName().equals(roomName)) {
+						for(Time time : room.getListTime()) {
+							if(time.getTime().equals(tim) && time.getTicket().getMovieName().equals(movieName)) {
+								for(int i = 0; i < time.getTicket().getListSeat().size(); i++) {
+									for(int j = 0; j < time.getTicket().getListSeat().get(i).size() - 1; j++) {
+										if(time.getTicket().getListSeat().get(i).get(j).getRow().equals(row)
+												&& time.getTicket().getListSeat().get(i).get(j).getNumber() == numSeat) {
+											int cnt = 0;
+											for(int k = 0; k < number; k++) {
+												if(checkTicket(cinemaName, movieName, tim, roomName, row, numSeat+k)) {
+													cnt++;
+												}
+											}
+											if(cnt == number) {
+												for(int k = 0; k < number; k++) {
+													removeTicket(cinemaName, movieName, tim, roomName, row, numSeat+k);
+													System.out.println("BAN DA DAT THANH CONG GHE " + row + (numSeat+k) + " THANH CONG !");
+												}
+											} else {
+												System.out.println("BAN DAT VE KHONG THANH CONG !");
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
 }
